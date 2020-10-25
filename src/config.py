4630584193b8ef1a9c905e0e8df80fbb6c2956ac
ialphaw <amirabbas.vaziri@gmail.com>
2020-10-25 22:43:51 +0330

    update /help message

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@ TOKEN = '1305679354:AAEpnDSabjTSN_mvBU68D00Ion0omyJ1h6w'
 endpoint = 'https://whispering-oasis-21115.herokuapp.com/'
 herokuapi = '23904f9c-5275-4333-a43c-f04c20949eeb'
 wlc_msg = '\nWelcome to @TeleMB_test.\nCommands available:\n/mute - mute the group\n/un_mute - un_mute the ' \
-          'group\nthis bot automatically delete messages contains link and delete invite and left messages in the ' \
+          'group\n/kick you reply this on someone\'s message and bot will remove it\nthis bot automatically delete ' \
+          'messages contains link and delete invite and left messages in the ' \
           'group\nthis bot also mute and un_mute the groups at a specific time '
-

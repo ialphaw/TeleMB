494224fb10afea1db1fe7ed9994f0d10d8d53726
ialphaw <amirabbas.vaziri@gmail.com>
2020-10-27 14:54:47 +0330

    update help message

--- src/config.py
+++ src/config.py
@@ -4,8 +4,6 @@ herokuapi = '23904f9c-5275-4333-a43c-f04c20949eeb'
 wlc_msg = '\nWelcome to @TeleMB_bot.\nCommands available:\n/mute - mute the group\n/un_mute - un_mute the ' \
           'group\n/kick you reply this on someone\'s message and bot will remove it\n/schedule_mute this command will ' \
           'auto mute your group and it gets like line one the time and line two the message\n/schedule_un_mute this ' \
-          'command will auto un mye your group and it gets like line one time and line two the message\nthis bot ' \
-          'automatically delete messages contains link and delete invite and left messages in the messages contains ' \
-          'link and delete invite and left messages in the messages contains link and delete invite and left messages ' \
-          'in the group\nthis bot also mute and un_mute the groups at a specific time'
+          'command will auto un mute your group and it gets like line one time and line two the message\n/allow_links allow you to send links\n/no_links delete evvery link in the group except admins\nthis bot ' \
+          'automatically delete messages contains link and delete invite and left messages in the group'
 creators_id = [1245192764]
